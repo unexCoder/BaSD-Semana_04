f3e52f76fe3138a7c8309dbd7116c8ab7d68cfa1
Luigi Tamagnini <bosca.music@gmail.com>
2021-10-25 16:31:46 -0300

    upgrade_ex_01

---
+++ exercises/01_variables_and_operators.js
@@ -0,0 +1,24 @@
+/*
+1) Variables y Operadores
+a) Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la
+suma de ambos números en una 3er variable.
+b) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er
+variable
+c) Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras
+    del string) guardando el resultado de la suma en una 3er variable (utilizar length).
+*/
+
+var number_1 = 20
+var number_2 = 50
+var number_3 = number_1 + number_2
+console.log(number_3)
+
+var firstName = "Luigi"
+var lastName = "Tamagnini"
+var coder = firstName+" "+lastName
+console.log(coder)
+
+var str_1 = "Software"
+var str_2 = "Development"
+var numChars = str_1.length + str_2.length
+console.log(numChars)
